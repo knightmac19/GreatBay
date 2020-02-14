fb69519578a791241fc7a52e29df90926812f5ff
Patrick Dunn <pmdunn78@gmail.com>
2020-02-13 21:14:05 -0500

    connection

--- greatBay.js
+++ greatBay.js
@@ -1,8 +1,38 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const util = require("util");
+var mysql = require("mysql");
+var sqlPassword = require("../support/mySQLpass");
 
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: sqlPassword,
+    database: "great_bay"
+  });
 
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    afterConnection();
+    
+  });
+
+  function afterConnection() {
+    connection.query("SELECT * FROM items", function(err, res) {
+        if (err) throw err;
+
+        console.log(res);
+        connection.end();
+    })
+}
 function initialPrompt() {
     // console.log("managerPrompt");
     return inquirer.prompt([
@@ -16,13 +46,13 @@ function initialPrompt() {
 }
 
 function bidPrompt() {
-
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: ""
-        }
-    ])
+    afterConnection();
+    // return inquirer.prompt([
+    //     {
+    //         type: "input",
+    //         name: ""
+    //     }
+    // ])
 }
 
 
@@ -32,9 +62,9 @@ async function init() {
         var initialRes = initialPrompt();
 
         if(initialRes === "Post an item") {
-            // function
+            console.log("post an item")
         } else {
-            // function
+            bidPrompt()
         }
         
     } catch (err) {
