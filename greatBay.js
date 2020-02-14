8068d6c14fd62b5447cded7afadf3d071141f234
Patrick Dunn <pmdunn78@gmail.com>
2020-02-13 20:54:06 -0500

    decluttered

--- greatBay.js
+++ greatBay.js
@@ -8,91 +8,34 @@ function initialPrompt() {
     return inquirer.prompt([
         {
             type: "list",
-            name: "newMember",
+            name: "action",
             message: "What do you want to do?",
             choices: ["Post an item", "Bid on an item"],
         }
     ])
 }
 
-function engineerPrompt() {
-    // console.log("engineerPrompt");
+function bidPrompt() {
+
     return inquirer.prompt([
         {
             type: "input",
-            name: "name",
-            message: "Please enter engineer's name:",
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Please enter engineer's email:",
-        },
-        {
-            type: "input",
-            name: "github",
-            message: "Please enter engineer's Github username:",
-        },
-        {
-            type: "confirm",
-            name: "anotherOne",
-            message: "Do you want to add another team member?",
-            default: true
-        },
-        {
-            type: "list",
-            name: "newMember",
-            message: "Which type of team member do you want to add?",
-            choices: ["Engineer", "Intern"],
-            when: function(answers) {
-                return answers.anotherOne;
-            }
+            name: ""
         }
-    ]);
+    ])
 }
-
-function internPrompt() {
-    // console.log("internPrompt");
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "Please enter intern's name:",
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Please enter intern's email:",
-        },
-        {
-            type: "input",
-            name: "school",
-            message: "Please enter intern's university",
-        },
-        {
-            type: "confirm",
-            name: "anotherOne",
-            message: "Do you want to add another team member?",
-            default: true
-        },
-        {
-            type: "list",
-            name: "newMember",
-            message: "Which type of team member do you want to add?",
-            choices: ["Engineer", "Intern"],
-            when: function(answers) {
-                return answers.anotherOne;
-            }
-        }
-    ]);
-}
-
 
 
 async function init() {
     // console.log("init() called");
     try {
-        initialPrompt();
+        var initialRes = initialPrompt();
+
+        if(initialRes === "Post an item") {
+            // function
+        } else {
+            // function
+        }
         
     } catch (err) {
         console.log(err);
